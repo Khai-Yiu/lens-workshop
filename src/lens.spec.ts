360fcd0dc95d3b82b8e992d4e25c46c8ceeb4b62
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-08-14 12:24:13 +0800

    RED/GREEN: lens > state copying behaviour > over should modify the state correctly and only update the necessary parts

--- src/lens.spec.ts
+++ src/lens.spec.ts
@@ -26,28 +26,106 @@ describe('lens', () => {
             address: { ...user.address, city: newCity }
         })
     );
-    it('view should retrieve the value using the lens', () => {
-        expect(view(nameLens, user)).toBe('Alice');
-        expect(view(cityLens, user)).toBe('Wonderland');
-    });
-    it('set should update the value using the lens', () => {
-        const updatedUser = set(nameLens, 'Gerald', user);
-        expect(updatedUser).not.toBe(user);
-        expect(updatedUser).toEqual({
-            name: 'Gerald',
-            address: { city: 'Wonderland', zip: '12345' }
+    describe('view', () => {
+        it('view should retrieve the value using the lens', () => {
+            expect(view(nameLens, user)).toBe('Alice');
+            expect(view(cityLens, user)).toBe('Wonderland');
         });
     });
-    it('over should modify the value using the lens', () => {
-        const updatedUser = over(
-            nameLens,
-            (name: string): string => name.toUpperCase(),
-            user
+    describe('set', () => {
+        it('set should update the value using the lens', () => {
+            const updatedUser = set(nameLens, 'Gerald', user);
+            expect(updatedUser).not.toBe(user);
+            expect(updatedUser).toEqual({
+                name: 'Gerald',
+                address: { city: 'Wonderland', zip: '12345' }
+            });
+        });
+    });
+    describe('over', () => {
+        it('over should modify the value using the lens', () => {
+            const updatedUser = over(
+                nameLens,
+                (name: string): string => name.toUpperCase(),
+                user
+            );
+            expect(updatedUser).not.toBe(user);
+            expect(updatedUser).toEqual({
+                name: 'ALICE',
+                address: { city: 'Wonderland', zip: '12345' }
+            });
+        });
+    });
+    describe('state copying behaviour', () => {
+        interface DeepState {
+            user: User;
+            meta: {
+                created: string;
+                modified: string;
+            };
+        }
+
+        const deepState: DeepState = {
+            user: {
+                name: 'Alice',
+                address: { city: 'Wonderland', zip: '12345' }
+            },
+            meta: { created: '2023-01-01', modified: '2024-01-01' }
+        };
+        const userLens = lens<DeepState, User>(
+            (state) => state.user,
+            (newUser, state) => ({ ...state, user: newUser })
         );
-        expect(updatedUser).not.toBe(user);
-        expect(updatedUser).toEqual({
-            name: 'ALICE',
-            address: { city: 'Wonderland', zip: '12345' }
+        const cityLens = lens<User, string>(
+            (user) => user.address.city,
+            (newCity, user) => ({
+                ...user,
+                address: { ...user.address, city: newCity }
+            })
+        );
+
+        const deepCityLens = lens<DeepState, string>(
+            (state) => view(cityLens, state.user),
+            (newCity, state) =>
+                set(userLens, set(cityLens, newCity, state.user), state)
+        );
+
+        it('set should copy the state correctly and only update the necessary parts', () => {
+            const updatedDeepState = set(deepCityLens, 'Oz', deepState);
+            expect(updatedDeepState).not.toBe(deepState);
+            expect(updatedDeepState).toEqual({
+                user: {
+                    name: 'Alice',
+                    address: { city: 'Oz', zip: '12345' }
+                },
+                meta: { created: '2023-01-01', modified: '2024-01-01' }
+            });
+            expect(updatedDeepState.user).not.toBe(deepState.user);
+            expect(updatedDeepState.user.address).not.toBe(
+                deepState.user.address
+            );
+            expect(updatedDeepState.meta).toBe(deepState.meta);
+        });
+        it('over should modify the state correctly and only update the necessary parts', () => {
+            const upperCaseCityState = over(
+                deepCityLens,
+                (city) => city.toUpperCase(),
+                deepState
+            );
+
+            expect(upperCaseCityState).not.toBe(deepState);
+            expect(upperCaseCityState).toEqual({
+                user: {
+                    name: 'Alice',
+                    address: { city: 'WONDERLAND', zip: '12345' }
+                },
+                meta: { created: '2023-01-01', modified: '2024-01-01' }
+            });
+            expect(upperCaseCityState.user).not.toBe(deepState.user);
+            expect(upperCaseCityState.user.address).not.toBe(
+                deepState.user.address
+            );
+            expect(upperCaseCityState.meta).toBe(deepState.meta);
         });
     });
 });
