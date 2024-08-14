cea4e94904db89db9b1e55c728a4192327fa644d
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-08-14 11:46:46 +0800

    RED: lens › over should modify the value using the lens

--- src/lens.spec.ts
+++ src/lens.spec.ts
@@ -1,4 +1,4 @@
-import lens, { view, set } from './lens';
+import lens, { view, set, over } from './lens';
 
 type Address = {
     city: string;
@@ -26,13 +26,28 @@ describe('lens', () => {
             address: { ...user.address, city: newCity }
         })
     );
-    expect(view(nameLens, user)).toBe('Alice');
     it('view should retrieve the value using the lens', () => {
+        expect(view(nameLens, user)).toBe('Alice');
         expect(view(cityLens, user)).toBe('Wonderland');
     });
     it('set should update the value using the lens', () => {
         const updatedUser = set(nameLens, 'Gerald', user);
         expect(updatedUser).not.toBe(user);
-        expect(view(nameLens, updatedUser)).toBe('Gerald');
+        expect(updatedUser).toEqual({
+            name: 'Gerald',
+            address: { city: 'Wonderland', zip: '12345' }
+        });
+    });
+    it('over should modify the value using the lens', () => {
+        const updatedUser = over(
+            nameLens,
+            (name: string): string => name.toUpperCase(),
+            user
+        );
+        expect(updatedUser).not.toBe(user);
+        expect(updatedUser).toEqual({
+            name: 'ALICE',
+            address: { city: 'Wonderland', zip: '12345' }
+        });
     });
 });
