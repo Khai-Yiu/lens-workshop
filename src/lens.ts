6804c284f1f804519cac3a17cb2108482fad0d4a
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-08-14 11:55:26 +0800

    GREEN: lens › over should modify the value using the lens

--- src/lens.ts
+++ src/lens.ts
@@ -8,6 +8,11 @@ interface Lens<S, A> {
 export const view = <S, A>(lens: Lens<S, A>, state: S): A => lens.get(state);
 export const set = <S, A>(lens: Lens<S, A>, valueToSet: A, state: S): S =>
     lens.set(valueToSet, state);
+export const over = <S, A>(
+    lens: Lens<S, A>,
+    fn: (value: A) => A,
+    state: S
+): S => lens.set(fn(lens.get(state)), state);
 
 const lens = <S, A>(
     getter: Getter<S, A>,
